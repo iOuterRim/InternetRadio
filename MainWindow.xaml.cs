3d9a6e89c7fb3cf8958fa610e670045c7b8581f6
iOuterRim <232771920+iOuterRim@users.noreply.github.com>
2025-09-17 11:08:09 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Media.Core;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace InternetRadio
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public ObservableCollection<RadioStation> Stations { get; set; } = new();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            // Set custom window icon
+            //WindowIconHelper.SetWindowIcon(this, "Assets\\AppIcon.ico");
+            ExtendsContentIntoTitleBar = true;
+            SetTitleBar(AppTitleBar);
+            StationsList.ItemsSource = Stations;
+            _ = LoadStationsAsync();
+        }
+        private async Task LoadStationsAsync()
+        {
+            using var client = new HttpClient();
+            var response = await client.GetStringAsync("https://de1.api.radio-browser.info/json/stations/topclick/10");
+            var stations = JsonSerializer.Deserialize<RadioStation[]>(response);
+            if (stations != null)
+            {
+                foreach (var station in stations)
+                    Stations.Add(station);
+            }
+        }
+
+        private void PlayButton_Click(object sender, RoutedEventArgs e)
+        {
+            var button = (Button)sender;
+            var streamUrl = button.Tag?.ToString();
+            if (!string.IsNullOrEmpty(streamUrl))
+            {
+                var mediaSource = MediaSource.CreateFromUri(new Uri(streamUrl));
+                Player.Source = mediaSource;
+            }
+        }
+    }
+    
+
+    public class RadioStation
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; } = string.Empty;
+
+        [JsonPropertyName("url_resolved")]
+        public string Url { get; set; } = string.Empty;
+    }
+
+
+}
---
+++ WindowIconHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Microsoft.UI.Xaml;
+using WinRT.Interop;
+
+namespace InternetRadio
+{
+    internal static class WindowIconHelper
+    {
+        private const int WM_SETICON = 0x80;
+        private const int ICON_BIG = 1;
+        private const int ICON_SMALL = 0;
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern IntPtr LoadImage(IntPtr hInstance, string lpszName, uint uType,
+            int cxDesired, int cyDesired, uint fuLoad);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern IntPtr SendMessage(IntPtr hWnd, int Msg, int wParam, IntPtr lParam);
+
+        private const uint IMAGE_ICON = 1;
+        private const uint LR_LOADFROMFILE = 0x00000010;
+
+        public static void SetWindowIcon(Window window, string iconPath)
+        {
+            var hwnd = WindowNative.GetWindowHandle(window);
+            var hIcon = LoadImage(IntPtr.Zero, iconPath, IMAGE_ICON, 0, 0, LR_LOADFROMFILE);
+
+            if (hIcon != IntPtr.Zero)
+            {
+                SendMessage(hwnd, WM_SETICON, ICON_BIG, hIcon);
+                SendMessage(hwnd, WM_SETICON, ICON_SMALL, hIcon);
+            }
+        }
+    }
+}
